38d6c601612dfd4bd2b26389c913e841bfe62e28
SaiChaitanya <SaiChaitanya@CEI3135.CEIIND.COM>
2019-03-06 16:33:34 +0530

    Changed Email 2

---
+++ Test Suites/Jenkins_sample.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Jenkins_sample</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>cb99b6be-9bd1-472b-9217-d9e49e6b48da</testSuiteGuid>
+   <testCaseLink>
+      <guid>8acfc40c-4b8a-4504-83aa-9e24cb341be8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
